ff23ad7ae4c5aab38c332d8f1e017003d7c8c804
Hugo Massing <hugo.massing@gmail.com>
2016-03-17 18:02:40 +0100

    feature add retrive artwork

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soundcloudArtworkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the soundcloudArtworkApp
+ */
+angular.module('soundcloudArtworkApp')
+  .controller('AboutCtrl', function () {
+  });
